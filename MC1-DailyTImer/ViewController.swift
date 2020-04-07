f7d21a43aebcc5928fbe39b3e2d1ffa4a2532108
muhamadVi <jayasriwi@gmail.com>
2020-04-07 20:40:02 +0700

    Initial Commit

---
+++ MC1-DailyTImer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC1-DailyTImer
+//
+//  Created by Muhamad Vicky on 07/04/20.
+//  Copyright © 2020 Muhamad Vicky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
