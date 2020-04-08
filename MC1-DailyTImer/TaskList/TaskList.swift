c5dd87b0b2f88ae9d90ceb5f23fb38a837b5cb30
Venus Dhammiko <dhammikovenus@gmail.com>
2020-04-08 19:09:14 +0700

    edit page 2: nambah table view sama edit nama

--- MC1-DailyTImer/TaskList/TaskList.swift
+++ MC1-DailyTImer/TaskList/TaskList.swift
@@ -8,13 +8,109 @@
 
 import UIKit
 
-class TaskList: UIViewController {
+class TaskList: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-
+    @IBOutlet weak var editNameBtn: UIButton!
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var taskTable: UITableView!
+    
+    /*
+    struct Task {
+             var taskName: String
+             var taskDesc: String
+    }*/
+    
+    let taskSections = ["Your Upcoming Task", "Your Completed Task"]
+    
+    let taskList = [[["name":"Coding", "desc":"Program Mini"], ["name":"Cuci", "desc":"piring gelas"]],
+                    [["name":"Belajar", "desc":"Mandarin"]]]
+    
+    /*
+    let upcomingTasks = [
+            Task(taskName: "Coding", taskDesc: "MC-1"),
+            Task(taskName: "Cuci", taskDesc: "Piring")
+    ]
+    
+    let completedTasks = [
+            Task(taskName: "Belajar", taskDesc: "Inggris")
+    ]*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        taskTable.dataSource = self
+        taskTable.delegate = self
+        
+        nameTxt.isEnabled = false
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.endEditing (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func endEditing (_ sender: UITapGestureRecognizer){
+        nameTxt.isEnabled = false
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.taskSections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        /*
+        let sectionName: String
+        switch section {
+            case 0:
+                sectionName = NSLocalizedString("Your Upcoming Task", comment: "mySectionName")
+            case 1:
+                sectionName = NSLocalizedString("Your Completed Task", comment: "myOtherSectionName")
+            default:
+                sectionName = ""
+        }
+        return sectionName
+        */
+        
+        return self.taskSections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          if taskList.indices.contains(section) {
+              return taskList[section].count
+          } else {
+              return 0
+          }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return  70
+    }
+      
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! taskTableViewCell
 
-        // Do any additional setup after loading the view.
+        
+        if taskList.indices.contains(indexPath.section) {
+            cell.taskNameLb?.text = taskList[indexPath.section][indexPath.row]["name"]
+            cell.taskDescLb?.text = taskList[indexPath.section][indexPath.row]["desc"]
+        } else {
+            cell.taskNameLb?.text = nil
+            cell.taskDescLb?.text = nil
+        }
+        
+        /*
+        let task = tasks[indexPath.row]
+        
+        cell.taskNameLb?.text = task.taskName
+        cell.taskDescLb?.text = task.taskDesc
+        */
+        
+        return cell
+    }
+    
+    
+    @IBAction func editNameClicked(_ sender: Any) {
+        nameTxt.isEnabled = true
+        nameTxt.becomeFirstResponder()
     }
     
     @IBAction func ToAddTask(_ sender: Any) {
@@ -37,17 +133,12 @@ class TaskList: UIViewController {
             }
         }
     }
+}
+
+class taskTableViewCell: UITableViewCell {
+      
+    @IBOutlet weak var taskNameLb: UILabel!
+    @IBOutlet weak var taskDescLb: UILabel!
+    @IBOutlet weak var priorityImage: UIImageView!
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
