cac27011614c3d520cd3b287355f8e567cb62942
Kevin Timotius <62124777+kevintimotius53@users.noreply.github.com>
2020-04-10 23:43:14 +0700

    updatean

--- MC1-DailyTImer/AddTask/AddTask.swift
+++ MC1-DailyTImer/AddTask/AddTask.swift
@@ -8,68 +8,138 @@
 
 import UIKit
 
-class AddTask: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class AddTask: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate, UITextFieldDelegate{
     
-    var taskName = ""
-    var taskDescription = ""
+    @IBOutlet weak var addTaskText: UITextField!
+    @IBOutlet weak var addTaskText2: UITextField!
+    @IBOutlet weak var estSessionField: UITextField!
+    @IBOutlet weak var timeSessionField: UITextField!
+    @IBOutlet weak var breakField: UITextField!
+    @IBOutlet weak var priorityField: UITextField!
     
-    struct CreateTask {
-        var toDoName : String
-        var toDoChoosen : String?
-    }
-    
-    let taskSection = [
-        CreateTask(toDoName: "Task Name"),
-        CreateTask(toDoName: "Task Description"),
-        CreateTask(toDoName: "Estimated Session",toDoChoosen: "Choose"),
-        CreateTask(toDoName: "Time per Session",toDoChoosen: "Choose"),
-        CreateTask(toDoName: "Breaktime per Session",toDoChoosen: "Choose"),
-        CreateTask(toDoName: "Priority",toDoChoosen: "Choose")
-    ]
-    
-    let estimatedSession = [1, 2, 3]
-    let timePerSession = [25,30,35]
-    let breakTimePerSession = [5,10,15]
-    let priority = ["Low", "Medium", "High"]
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Create New Task"
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return taskSection.count
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if indexPath.row < 2{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "addNewTask", for: indexPath) as! CustomTaskCell
-            let cons1 = taskSection[indexPath.row]
-        }
-        if indexPath.row > 1{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "addNewTask-2", for: indexPath) as! CustomTaskCell
-            let cons2 = taskSection[indexPath.row]
-            cell.addTaskPicker?.text = cons2.toDoName
-            
+    var newPickerView = UIPickerView()
+    var currentTextField = UITextField()
+
+    var taskName: String = ""
+    var taskDescription: String = ""
+    var estimatedSession = 0
+    var timePerSession = 0
+    var breakTimePerSession = 0
+    var priority = ""
+    
+    var estimatedSessionArr = ["1","2","3"]
+    var timePerSessionArr = ["20","25","30"]
+    var breakTimePerSessionArr = ["5","10","15"]
+    var priorityArr = ["Low","Medium","High"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if currentTextField == estSessionField{
+            return estimatedSessionArr.count
+        }else if currentTextField == timeSessionField{
+            return timePerSessionArr.count
+        }else if currentTextField == breakField{
+            return breakTimePerSessionArr.count
+        }else if currentTextField == priorityField{
+            return priorityArr.count
+        }else{
+            return 0
         }
         
-        return UITableViewCell()
-}
+    }
     
-   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if currentTextField == estSessionField{
+            return estimatedSessionArr[row]
+        }else if currentTextField == timeSessionField{
+            return timePerSessionArr[row]
+        }else if currentTextField == breakField{
+            return breakTimePerSessionArr[row]
+        }else if currentTextField == priorityField{
+            return priorityArr[row]
+        }else{
+            return ""
+        }
+    }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if currentTextField == estSessionField{
+            estSessionField.text = estimatedSessionArr[row]
+        }else if currentTextField == timeSessionField{
+            timeSessionField.text = timePerSessionArr[row]
+        }else if currentTextField == breakField{
+            breakField.text = breakTimePerSessionArr[row]
+        }else if currentTextField == priorityField{
+            priorityField.text = priorityArr[row]
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        currentTextField = textField
+        if currentTextField == addTaskText{
+            textField.resignFirstResponder()
+        }
+        if currentTextField == addTaskText2{
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        newPickerView.dataSource = self
+        newPickerView.delegate = self
+        
+        newPickerView.backgroundColor = UIColor(red: 207/255, green: 212/255, blue: 218/255, alpha: 1)
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        //toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector (categoryDoneClicked))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: #selector (categoryDoneClicked))
+        
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: true)
+        toolBar.isUserInteractionEnabled = true
+                
+        currentTextField = textField
+        currentTextField.inputAccessoryView = toolBar
+        if currentTextField == estSessionField{
+            estSessionField.inputView = newPickerView
+        }else if textField == timeSessionField{
+            timeSessionField.inputView = newPickerView
+        }else if textField == breakField{
+            breakField.inputView = newPickerView
+        }else if textField == priorityField{
+            priorityField.inputView = newPickerView
+        }
+    }
+    
+    @objc func categoryDoneClicked() {
+        currentTextField.inputView = newPickerView
+        self.view.endEditing(true)
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
+    @IBAction func doneButton(_ sender: Any) {
+        addTaskText.text = taskName
+        print(taskName)
+        print(taskDescription)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -80,12 +150,4 @@ class AddTask: UIViewController, UITableViewDelegate, UITableViewDataSource{
     }
     */
 
-}
-
-class CustomTaskCell: UITableViewCell{
-    
-    @IBOutlet weak var addTaskText: UITextField!
-    @IBOutlet weak var addTaskPicker: UILabel!
-    @IBOutlet weak var pickerViewChoosen: UILabel!
-    
 }
